bd5d608d00b2eedab3c1f676cb1f32611a19087a
Aakanksha-Maheshwari <aamahesh@syr.edu>
2024-11-30 19:20:17 +0000

    v35

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,14 @@ import requests
 import json
 import openai
 from crewai import Agent
-
-# ChromaDB imports
+import streamlit as st
+import requests
+import json
+__import__('pysqlite3')
+import sys,os
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import chromadb
+from chromadb.config import Settings
 
 # Initialize ChromaDB Persistent Client
 client = chromadb.PersistentClient()
